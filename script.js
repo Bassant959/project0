a74d96e2ec905283c7bba6953ce79adf51e870ef
Bassant959 <bsntahmd959@gmail.com>
2026-06-18 23:24:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,103 @@
-// لما يفتح صفحة Contact.html يجيب اسم المنتج من الرابط
-window.onload = function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const product = urlParams.get('product');
-    if (product) {
-      document.getElementById('product').value = product;
+const products = [
+
+    {
+    name:"Laptop Bag",
+    price:650,
+    image:"images/Laptop Bag.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 1",
+    price:650,
+    image:"images/Laptop Bag 1.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 2",
+    price:650,
+    image:"images/Laptop Bag 2.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 3",
+    price:750,
+    image:"images/Laptop Bag 3.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 4",
+    price:700,
+    image:"images/Laptop Bag 4.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 5",
+    price:600,
+    image:"images/Laptop Bag 5.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 6",
+    price:600,
+    image:"images/Laptop Bag 6.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 7",
+    price:650,
+    image:"images/Laptop Bag 7.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 8",
+    price:750,
+    image:"images/Laptop Bag 8.jpeg"
+    },
+    
+    {
+    name:"Laptop Bag 9",
+    price:600,
+    image:"images/Laptop Bag 9.jpeg"
     }
-  };
-  
-  // لما يضغط على زرار Send Order يفتح واتساب برسالة جاهزة
-  document.getElementById('orderForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-  
-    const name = document.getElementById('name').value;
-    const phone = document.getElementById('phone').value;
-    const address = document.getElementById('address').value;
-    const product = document.getElementById('product').value;
-  
-    // رقم واتسابك (غيريه برقمك الحقيقي)
-    const whatsappNumber = "201234567890"; 
-  
-    const message = `New Order:%0AName: ${name}%0APhone: ${phone}%0AAddress: ${address}%0AProduct: ${product}`;
-    const url = `https://wa.me/${whatsappNumber}?text=${message}`;
-  
-    window.open(url, '_blank');
-  });
-  
+    
+    ];
+    
+    const container = document.querySelector(".products");
+    
+    products.forEach(product => {
+    
+    container.innerHTML += `
+    <div class="card">
+    
+    <img src="${product.image}" alt="${product.name}">
+    
+    <div class="info">
+    
+    <h3>${product.name}</h3>
+    
+    <div class="price">
+    ${product.price} EGP
+    </div>
+    
+    <button class="buy"
+    onclick="buyNow('${product.name}','${product.price}')">
+    
+    Buy Now
+    
+    </button>
+    
+    </div>
+    
+    </div>
+    `;
+    
+    });
+    
+    function buyNow(name,price){
+    
+    localStorage.setItem("product",name);
+    localStorage.setItem("price",price);
+    
+    window.location.href="checkout.html";
+    
+    }
